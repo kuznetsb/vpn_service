600a98de151d9877a6a6a6ef992fa92ee2538930
kuznetsb <kuznetsbog@gmail.com>
2023-11-27 21:50:12 +0200

    gitignore and settings modified

--- vpn_service/settings.py
+++ vpn_service/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
