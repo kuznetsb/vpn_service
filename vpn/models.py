e11fd12dc152dff05d6a91a90305ac4725d11dd6
kuznetsb <kuznetsbog@gmail.com>
2023-11-28 15:04:31 +0200

    created models for vpn module

--- vpn/models.py
+++ vpn/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255, unique=True)
+
+
+class Statistics(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="statistics"
+    )
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name="statistics")
+    transitions_number = models.PositiveIntegerField(default=0)
+    data_volume_upload = models.FloatField(
+        help_text="Обʼєм даних який було відправлено (в МБ)",
+        default=0.0,
+        validators=[MinValueValidator(0.0)],
+    )
+    data_volume_downloaded = models.FloatField(
+        help_text="Обʼєм даних який було завантажено (в МБ)",
+        default=0.0,
+        validators=[MinValueValidator(0.0)],
+    )
+
+    class Meta:
+        unique_together = [("user", "site")]
