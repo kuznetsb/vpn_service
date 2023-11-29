583feb8dc828c485bde4ebc2e3081f1fb58a05a1
kuznetsb <kuznetsbog@gmail.com>
2023-11-29 13:17:56 +0200

    site creation modified

--- vpn/forms.py
+++ vpn/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
 from vpn.models import Site
 
@@ -16,6 +18,19 @@ class SiteCreateForm(forms.ModelForm):
     class Meta:
         model = Site
         fields = ["name", "url"]
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        url_data = self.cleaned_data["url"]
+        unified_url = url_data.strip("/").replace("https", "http")
+        if Site.objects.filter(url=unified_url, author=self.request.user).exists():
+            raise ValidationError("You already added this site")
+
+        self.cleaned_data["url"] = unified_url
+        return self.cleaned_data
 
 
 class SiteNameUpdateForm(forms.ModelForm):
